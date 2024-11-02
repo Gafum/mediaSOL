234d5c38be72690c4e514ffc7612974a10881581
Marchuk Dima <marcukdyma@gmail.com>
2024-11-02 18:00:26 +0100

    optimization for contact screen

--- src/Screens/Contact/Contact.tsx
+++ src/Screens/Contact/Contact.tsx
@@ -1,14 +1,58 @@
+import { LandPlot, Mail, Phone, SquareChartGantt } from "lucide-react";
+
+interface IParagraphWithTitleProps {
+   title: string;
+   text: string;
+   icon: JSX.Element;
+}
+
+function ParagraphWithTitle({ icon, title, text }: IParagraphWithTitleProps): JSX.Element {
+   return (
+      <p className="w-full flex justify-start gap-2">
+         <strong className="flex gap-1">{icon}{title}:</strong>
+         <span>{text}</span>
+      </p>
+   )
+}
+
+const paragraphsInContacts: IParagraphWithTitleProps[] = [
+   {
+      title: "Adresse",
+      text: "Hauptstraße 123, 46535 HaupStadt, Deutschland",
+      icon: <LandPlot width={20} />
+   },
+   {
+      title: "Telefon",
+      text: "+49 000 0000000",
+      icon: <Phone width={20} />
+   },
+   {
+      title: "E-Mail",
+      text: "kontakt@mediasol.de",
+      icon: <Mail width={20} />
+   },
+   {
+      title: "Öffnungszeiten",
+      text: "Montag - Freitag, 9:00 - 18:00 Uhr",
+      icon: <SquareChartGantt width={20} />
+   },
+]
+
 function Contact(): JSX.Element {
+
    return (
       <div>
-         <h2>Unsere Kontaktinformationen</h2>
-         <p>Wir freuen uns, von Ihnen zu hören! Bitte kontaktieren Sie uns über die untenstehenden Informationen oder das Formular.</p>
+         <div className="w-full flex justify-start items-center gap-5 min-h-[180px]">
+            <img src="./mainIcon.svg" alt="M" className="w-1/2 max-w-[300px]" />
+            <div className="w-full flex flex-col gap-2">
+               <h1 className="font-bold text-[80px]">MediaSOL</h1>
+            </div>
+         </div>
 
-         <div className="Contact__info">
-            <p><strong>Adresse:</strong> Hauptstraße 123, 46535 HaupStadt, Deutschland</p>
-            <p><strong>Telefon:</strong> +49 000 0000000</p>
-            <p><strong>E-Mail:</strong> kontakt@mediasol.de</p>
-            <p><strong>Öffnungszeiten:</strong> Montag - Freitag, 9:00 - 18:00 Uhr</p>
+         <div className="mt-4 flex flex-col w-full gap-1">
+            {paragraphsInContacts.map((paragraphData) => {
+               return <ParagraphWithTitle {...paragraphData} />
+            })}
          </div>
       </div>
    );
