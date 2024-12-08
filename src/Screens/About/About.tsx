33249509732d6210866729f2d523bd69ab060d48
Marchuk Dmytro <marcukdyma@gmail.com>
2024-12-08 20:54:16 +0100

    about page was redesigned

--- src/Screens/About/About.tsx
+++ src/Screens/About/About.tsx
@@ -1,21 +1,41 @@
-import { ReactNode } from "react"
+import { ReactNode } from "react";
+import { screenList } from "../../Routing/RoutingList";
+import { useNavigate } from "react-router-dom";
+import CustomBtn from "../../UI/CustomBtn/CustomBtn";
 
 interface IAboutSectionData {
-   name: string
-   title: string
-   description: string | string[]
+   name: string;
+   title: string;
+   description: string | { title: string; text: string }[];
 }
 
 const aboutSectionData: IAboutSectionData[] = [
    {
+      name: "shortDescription",
+      title: " Über Uns",
+      description:
+         "MediaSOL GmbH ist ein IT-Beratungsunternehmen, das Firmen berät, mit passenden Medien ausstattet und auf Wunsch die technische Wartung übernimmt",
+   },
+   {
       name: "services",
       title: "Leistungsspektrum",
       description: [
-         "Verkauf von technischen Produkten (PCs, Apple Produkte, Drucker, Scanner, Server, Beamer, usw.)",
-         "Beratung von Firmen und Schulen",
-         "Wartung der Rechner und Server",
-         "Netzwerk-Einrichtung",
-         "Technischer Support",
+         {
+            title: "Verkauf von technischen Produkten",
+            text: "PCs, Apple-Produkte, Drucker, Scanner, Server, Beamer und vieles mehr – wir bieten hochwertige Technik für Ihre Bedürfnisse.",
+         },
+         {
+            title: "Beratung für Firmen und Schulen",
+            text: "Maßgeschneiderte Lösungen, die perfekt zu Ihren Anforderungen passen.",
+         },
+         {
+            title: "Technische Wartung",
+            text: "Wartung von Rechnern und Servern für einen reibungslosen Betrieb.",
+         },
+         {
+            title: "Netzwerk-Einrichtung",
+            text: "Effiziente Netzwerke für Ihre IT-Infrastruktur.",
+         },
       ],
    },
    {
@@ -26,7 +46,7 @@ const aboutSectionData: IAboutSectionData[] = [
    },
    {
       name: "ourColleagues",
-      title: "Mitarbeiter",
+      title: "Unser Team",
       description:
          "Unser Team besteht aus engagierten Fachleuten, die sich in verschiedenen Bereichen spezialisiert haben. Dazu gehören mehrere erfahrene Verkäuferinnen und Verkäufer, die ihre Expertise aus dem früheren Betrieb des Unternehmens übernommen haben. Hinzu kommen sechs technische Spezialisten, darunter IT-System-Elektroniker, Servicetechniker und Fachinformatiker, die für die technische Unterstützung unserer Kunden sorgen. Das Team wird in naher Zukunft durch drei zusätzliche Techniker erweitert. Zusätzlich bieten wir jungen Talenten Ausbildungsplätze als IT-System-Elektroniker und Fachinformatiker an.",
    },
@@ -34,9 +54,15 @@ const aboutSectionData: IAboutSectionData[] = [
       name: "location",
       title: "Standort und internationale Kontakte",
       description:
-         "MediaSOL ist ein Unternehmen mit internationalen Kontakten nach England, Spanien, China und den USA. Der Firmenstandort sowie der ehemalige Elektrogroßhandel befinden sich in einem Gewerbegebiet in Hauptstadt.",
+         "Unser Hauptsitz befindet sich in einem Gewerbegebiet der Hauptstadt. Wir pflegen internationale Kontakte nach England, Spanien, China und den USA, um unseren Kunden die besten Lösungen anzubieten.",
    },
-]
+   {
+      name: "workWithUs",
+      title: "Bereit, mit uns zu arbeiten?",
+      description:
+         "Kontaktieren Sie uns, um mehr über unsere Dienstleistungen zu erfahren. Wir freuen uns darauf, von Ihnen zu hören!",
+   },
+];
 
 function SectionAboutPage({
    title,
@@ -44,41 +70,63 @@ function SectionAboutPage({
 }: IAboutSectionData): JSX.Element {
    function createDescription(): ReactNode {
       if (typeof description == "string") {
-         return description
+         return <p className="text-sm">{description}</p>;
       } else {
-         return description.map((text) => <li key={text}> {text}</li>)
+         return (
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-3 mb-2">
+               {description.map((text) => (
+                  <div
+                     key={text.title}
+                     className="bg-primaryLightGrey p-4 rounded-md"
+                  >
+                     <h3 className="text-md font-semibold">{text.title}</h3>
+                     <p className="text-sm mt-1">{text.text}</p>
+                  </div>
+               ))}
+            </div>
+         );
       }
    }
 
    return (
-      <section className={"About__" + title}>
-         <h2 className="text-xl font-semibold pt-2">{title}</h2>
-         <p className="text-sm">{createDescription()}</p>
+      <section className={"About__" + title + " mt-6"}>
+         <h2 className="text-2xl font-semibold text-left mb-2">{title}</h2>
+         {createDescription()}
       </section>
-   )
+   );
 }
 
 function About() {
+   const navigate = useNavigate();
+
    return (
-      <div className="max-w-[800px] m-auto flex flex-col gap-4">
+      <div className="max-w-[800px] m-auto flex flex-col">
          <section className="About__description flex gap-4 justify-start items-center flex-col lg:flex-row">
             <img
                src="/mainIcon.svg"
                alt="M"
                className="w-full max-w-[200px] lg:w-1/2"
             />
-            <p className="text-sm">
-               <strong className="text-semibold">MediaSOL GmbH</strong> ist ein
-               IT-Beratungsunternehmen, das Firmen berät, mit passenden Medien
-               ausstattet und auf Wunsch die technische Wartung übernimmt.
-            </p>
+            <div className="text-left py-6">
+               <h1 className="text-2xl font-bold">Über MediaSOL GmbH</h1>
+               <p className="text-sm text-gray-600">
+                  Ihr zuverlässiger Partner für IT-Beratung, technische Lösungen
+                  und Innovation.
+               </p>
+            </div>
          </section>
 
          {aboutSectionData.map((sectionData) => {
-            return <SectionAboutPage {...sectionData} key={sectionData.name} />
+            return <SectionAboutPage {...sectionData} key={sectionData.name} />;
          })}
+
+         <CustomBtn
+            onClick={() => navigate(screenList.contact.path)}
+            btnText="Zur Kontaktseite"
+            className="mt-6"
+         />
       </div>
-   )
+   );
 }
 
-export default About
+export default About;
