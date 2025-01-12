917db33cbc3670165541aa34ca9a887b969dff1f
Marchuk Dmytro <marcukdyma@gmail.com>
2025-01-12 16:16:36 +0100

    added new carousel swiper

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { MainRouter } from "./Routing/Routing";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import "swiper/css";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
