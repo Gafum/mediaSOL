d0a8aa8c6ea62c1b99637772fd836a654b78b1be
Marchuk Dmytro <marcukdyma@gmail.com>
2025-02-14 23:26:35 +0100

    added clearFavoritesList Btn in Cart Page

--- src/Screens/Cart/Cart.tsx
+++ src/Screens/Cart/Cart.tsx
@@ -1,3 +1,28 @@
+import { useFavoritesStore } from "../../Store/FavoritesStore";
+import { CustomBtn } from "../../UI/CustomBtn/CustomBtn";
+
 export const Cart = (): JSX.Element => {
-   return <>Cart</>;
+   const clearFavoritesList = useFavoritesStore(
+      (state) => state.clearFavoritesList
+   );
+   const favoritesList = useFavoritesStore((state) => state.favoritesList);
+   return (
+      <section>
+         {" "}
+         <div className="flex justify-between">
+            {" "}
+            <h1>Cart</h1>{" "}
+            <CustomBtn
+               btnText={"Clear Favorites List"}
+               onClick={() => clearFavoritesList()}
+            />{" "}
+         </div>{" "}
+         <div>
+            {" "}
+            {favoritesList.map((elem) => (
+               <div>{elem}</div>
+            ))}{" "}
+         </div>{" "}
+      </section>
+   );
 };
