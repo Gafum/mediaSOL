b976b6462918c249490435a26593583a6aa0367f
Marchuk Dmytro <marcukdyma@gmail.com>
2025-03-14 19:59:58 +0100

    fixing services Section on home page for md screens

--- src/Screens/Home/MyComponents/ServicesSection.tsx
+++ src/Screens/Home/MyComponents/ServicesSection.tsx
@@ -1,22 +1,38 @@
 import { ChevronRight } from "lucide-react";
 import { screenList } from "../../../Routing/RoutingList";
 import { Link } from "react-router-dom";
+import { twMerge } from "tailwind-merge";
+
+const ServicesDescription = ({
+   title,
+   className,
+}: {
+   title: string;
+   className?: string;
+}): JSX.Element => {
+   return (
+      <div
+         className={twMerge(
+            "text__block absolute max-w-[350px] w-1/2",
+            className
+         )}
+      >
+         <h1 className="text-white text-lg lg:text-2xl ">{title}</h1>
+         <button className="h-10 w-10  bg-[#ffffff44] transition-colors hover:bg-[#ffffff55] duration-300 flex justify-center items-center mt-3 rounded-[4px]">
+            <ChevronRight color="#ffffff" />
+         </button>
+      </div>
+   );
+};
 
 export const ServicesSection = (): JSX.Element => {
    return (
-      <section className="flex gap-4 mt-9">
+      <section className="flex flex-col md:flex-row gap-4 mt-9">
          {/* Left Side */}
-         <div className="w-1/2 flex flex-col gap-4">
+         <div className="w-full md:w-1/2 flex flex-col gap-4">
             <Link to={screenList.consultationService.path}>
                <div className="text__bloc relative bg-primaryDarkGrey p-5 rounded-lg">
-                  <div className="text__block absolute max-w-[350px] w-1/2">
-                     <h1 className=" text-white text-2xl">
-                        Beratung von Firmen und Schulen
-                     </h1>
-                     <div className="h-10 w-10 bg-[#ffffff44] flex justify-center items-center mt-3 rounded-[4px] duration-300 transition-colors hover:bg-[#ffffff55]">
-                        <ChevronRight color="#ffffff" />
-                     </div>
-                  </div>
+                  <ServicesDescription title="Beratung von Firmen und Schulen" />
                   <img
                      src={
                         import.meta.env.BASE_URL +
@@ -30,14 +46,10 @@ export const ServicesSection = (): JSX.Element => {
 
             <Link to={screenList.networkSetupService.path}>
                <div className="text__bloc relative bg-primaryPink flex flex-col gap-4 p-5 rounded-lg">
-                  <div className="text__block absolute max-w-350px] w-1/2">
-                     <h1 className="text-white text-2xl">
-                        Netzwerkeinstellungen und Einrichtung
-                     </h1>
-                     <button className="h-10 w-10 bg-[#ffffff44] flex justify-center items-center mt-3 rounded-[4px] duration-300 transition-colors hover:bg-[#ffffff55]">
-                        <ChevronRight color="#ffffff" />
-                     </button>
-                  </div>
+                  <ServicesDescription
+                     title="Netzwerkeinstellungen und Einrichtung"
+                     className="md:w-2/5"
+                  />
                   <img
                      src={
                         import.meta.env.BASE_URL +
@@ -51,45 +63,37 @@ export const ServicesSection = (): JSX.Element => {
          </div>
 
          {/* Right Side */}
-         <div className="w-1/2 flex flex-col gap-4">
+         <div className="w-full md:w-1/2 flex flex-col gap-4">
             <Link to={screenList.maintenanceService.path}>
                <div className="relative bg-primaryDarkGrey flex flex-col gap-4 p-5 rounded-lg">
-                  <div className="text__block absolute max-w-[350px] w-2/5">
-                     <h1 className="text-white text-2xl">
-                        Wartung der Rechner und Server
-                     </h1>
-                     <button className="h-10 w-10 bg-[#ffffff44] transition-colors hover:bg-[#ffffff55] duration-300 flex justify-center items-center mt-3 rounded-[4px]">
-                        <ChevronRight color="#ffffff" />
-                     </button>
-                  </div>
+                  <ServicesDescription
+                     title="Wartung der Rechner und Server"
+                     className="md:w-2/5"
+                  />
                   <img
                      src={
                         import.meta.env.BASE_URL +
                         "/SVG/Services/Maintenance.svg"
                      }
                      alt="Maintenance"
-                     className="w-3/5 ml-auto"
+                     className="w-1/2 md:w-3/5 ml-auto "
                   />
                </div>
             </Link>
 
             <Link to={screenList.technicalSupportService.path}>
                <div className="relative bg-primaryBlue flex flex-col gap-4 p-5 rounded-lg">
-                  <div className="text__block absolute max-w-[350px] w-2/5">
-                     <h1 className="text-white text-2xl">
-                        Technischer Support und Service
-                     </h1>
-                     <button className="h-10 w-10 bg-[#ffffff44] transition-colors hover:bg-[#ffffff55] duration-300 flex justify-center items-center mt-3 rounded-[4px]">
-                        <ChevronRight color="#ffffff" />
-                     </button>
-                  </div>
+                  <ServicesDescription
+                     title="Technischer Support und Service"
+                     className="lg:w-2/5 md:min-w-[195px]"
+                  />
                   <img
                      src={
                         import.meta.env.BASE_URL +
                         "/SVG/Services/TechnicalSupport.svg"
                      }
                      alt="TechnicalSupport"
-                     className="w-2/5 ml-auto"
+                     className="w-1/2 md:w-2/5 ml-auto"
                   />
                </div>
             </Link>
